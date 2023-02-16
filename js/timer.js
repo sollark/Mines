9b612db146e374cde22ac066bd6c03e1cc992e39
Andrey Solonikov <andreysol@yahoo.com>
2023-02-16 18:06:39 +0200

    cleaned up

--- js/timer.js
+++ js/timer.js
@@ -1,65 +1,64 @@
-'use strict';
+'use strict'
 
-let gTimerInterval = null;
-let gStartTime = 0;
-let gDurationTime = 0;
+let gTimerInterval = null
+let gStartTime = 0
+let gDurationTime = 0
 
 function timer() {
-  var elTimer = document.querySelector('.timer span');
+  var elTimer = document.querySelector('.timer span')
 
-  gStartTime = Date.now();
+  gStartTime = Date.now()
 
   const interval = setInterval(function () {
-    var diff = Date.now() - gStartTime;
-    elTimer.innerText = ('' + diff).toMMSS();
-  }, 100);
+    var diff = Date.now() - gStartTime
+    elTimer.innerText = ('' + diff).toMMSS()
+  }, 100)
 
-  return interval;
+  return interval
 }
 
 function clearTimer() {
-  gStartTime = 0;
-  gDurationTime = 0;
+  gStartTime = 0
+  gDurationTime = 0
 
-  var elTimer = document.querySelector('.timer span');
-  elTimer.innerText = '00:00';
+  var elTimer = document.querySelector('.timer span')
+  elTimer.innerText = '00:00'
 }
 
 function getGameTime() {
-  return Date.now() - gStartTime;
+  return Date.now() - gStartTime
 }
 
 function startTimer() {
-  clearTimer();
-  gTimerInterval = timer();
+  clearTimer()
+  gTimerInterval = timer()
 }
 
 function stopTimer() {
-  clearInterval(gTimerInterval);
-  gTimerInterval = null;
+  clearInterval(gTimerInterval)
+  gTimerInterval = null
 
-  gDurationTime = getGameTime();
+  gDurationTime = getGameTime()
 
-  return gDurationTime;
+  return gDurationTime
 }
 
 String.prototype.toMMSS = function () {
-  const dateValue = parseInt(this, 10); // don't forget the second param
-  const date = new Date(dateValue);
+  const dateValue = parseInt(this, 10) // don't forget the second param
+  const date = new Date(dateValue)
 
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var seconds = date.getSeconds();
-  var milliseconds = date.getMilliseconds();
+  var hours = date.getHours()
+  var minutes = date.getMinutes()
+  var seconds = date.getSeconds()
 
   if (hours < 10) {
-    hours = '0' + hours;
+    hours = '0' + hours
   }
   if (minutes < 10) {
-    minutes = '0' + minutes;
+    minutes = '0' + minutes
   }
   if (seconds < 10) {
-    seconds = '0' + seconds;
+    seconds = '0' + seconds
   }
-  return minutes + ':' + seconds;
-};
+  return minutes + ':' + seconds
+}
--- js/util.js
+++ js/util.js
@@ -1,99 +1,93 @@
-'use strict';
+'use strict'
 
 // MODEL MATRIX
 function buildBoard(rows, cols, mines) {
-  const mat = [];
+  const mat = []
   for (var i = 0; i < rows; i++) {
-    const row = [];
+    const row = []
     for (var j = 0; j < cols; j++) {
-      row.push(createCell());
+      row.push(createCell())
     }
-    mat.push(row);
+    mat.push(row)
   }
 
-  return mat;
+  return mat
 }
 
 // DOM MATRIX
 function renderBoard(board) {
-  const elBoard = document.querySelector('.board');
+  const elBoard = document.querySelector('.board')
 
-  var strHTML = '';
+  var strHTML = ''
   for (var i = 0; i < board.length; i++) {
-    strHTML += '<tr>\n';
+    strHTML += '<tr>\n'
 
     for (var j = 0; j < board[0].length; j++) {
-      let cellClass = `cell cell-${i}-${j}`;
-
-      strHTML += `\t<td class="cell ${cellClass}"  onclick="cellClicked(this,${i},${j})" >\n`;
-
-      strHTML += '\t</td>\n';
+      let cellClass = `cell cell-${i}-${j}`
+      strHTML += `\t<td class="cell ${cellClass}"  onclick="cellClicked(this,${i},${j})" >\n`
+      strHTML += '\t</td>\n'
     }
-    strHTML += '</tr>\n';
+    strHTML += '</tr>\n'
   }
 
-  elBoard.innerHTML = strHTML;
+  elBoard.innerHTML = strHTML
 }
 
 function getElByLocation(location) {
-  const cellSelector = '.' + getLocationClassName(location); // cell-i-j
-  return document.querySelector(cellSelector);
+  const cellSelector = '.' + getLocationClassName(location) // cell-i-j
+  return document.querySelector(cellSelector)
 }
 
 function setClassTo(location, value) {
-  const cellSelector = '.' + getLocationClassName(location); // cell-i-j
-  // console.log('cellSelector:', cellSelector);
-  // console.log('value:', value);
-  const elCell = document.querySelector(cellSelector);
-  elCell.classList.add(value);
+  const cellSelector = '.' + getLocationClassName(location) // cell-i-j
+  const elCell = document.querySelector(cellSelector)
+  elCell.classList.add(value)
 }
 
 // Gets coordinates {i, j } and returns a class name for those coordinates
 function getLocationClassName(location) {
-  const cellClass = 'cell-' + location.i + '-' + location.j;
-  return cellClass;
+  const cellClass = 'cell-' + location.i + '-' + location.j
+  return cellClass
 }
 
 // Convert a coordinate {i, j} to a selector and render a DOM value in that element
 function renderCell(location, value) {
-  const cellSelector = '.' + getLocationClassName(location); // cell-i-j
+  const cellSelector = '.' + getLocationClassName(location) // cell-i-j
 
-  const elCell = document.querySelector(cellSelector);
-  elCell.innerHTML = value;
+  const elCell = document.querySelector(cellSelector)
+  elCell.innerHTML = value
 }
 
 function addToCellValue(location, value) {
-  const cellSelector = '.' + getLocationClassName(location); // cell-i-j
-  // console.log('cellSelector:', cellSelector);
-  // console.log('value:', value);
-  const elCell = document.querySelector(cellSelector);
-  elCell.innerHTML += value;
+  const cellSelector = '.' + getLocationClassName(location) // cell-i-j
+  const elCell = document.querySelector(cellSelector)
+  elCell.innerHTML += value
 }
 
 // Find neighbors
 function getNeighborsAround(mat, pos) {
-  var neighbors = [];
+  var neighbors = []
   for (var i = pos.i - 1; i <= pos.i + 1; i++) {
-    if (i < 0 || i >= mat.length) continue;
+    if (i < 0 || i >= mat.length) continue
     for (var j = pos.j - 1; j <= pos.j + 1; j++) {
-      if (j < 0 || j >= mat[i].length) continue;
-      if (i === pos.i && j === pos.j) continue;
-      neighbors.push({ i: i, j: j });
+      if (j < 0 || j >= mat[i].length) continue
+      if (i === pos.i && j === pos.j) continue
+      neighbors.push({ i: i, j: j })
     }
   }
-  return neighbors;
+  return neighbors
 }
 
 // Returns random integer
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 // Enable event on right mouse click
 window.oncontextmenu = (e) => {
-  e.preventDefault();
+  e.preventDefault()
 
   e.target.onclick
     ? e.target.onclick()
-    : e.originalTarget.offsetParent.onclick();
-};
+    : e.originalTarget.offsetParent.onclick()
+}
